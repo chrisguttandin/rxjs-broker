ead445be7a9c18f0a990cae0ef7c0cf62799219f
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-01 00:49:06 +0200

    set up travis tests

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -2,37 +2,65 @@
 
 module.exports = function (config) {
 
-    config.set({
+    /* eslint-disable indent */
+    var configuration = {
 
-        browsers: [
+            files: [
+                '../../src/module.js',
+                {
+                    included: false,
+                    pattern: '../../src/**/!(module).js',
+                    served: false,
+                    watched: true,
+                },
+                '../../test/integration/**/*.js',
+                '../../test/unit/**/*.js'
+            ],
+
+            frameworks: [
+                'browserify',
+                'mocha',
+                'sinon-chai' // implicitly uses chai too
+            ],
+
+            preprocessors: {
+                '../../src/module.js': 'browserify',
+                '../../test/integration/**/*.js': 'browserify',
+                '../../test/unit/**/*.js': 'browserify'
+            }
+
+        };
+    /* eslint-enable indent */
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = [
+            'ChromeSauceLabs',
+            'FirefoxSauceLabs'
+        ];
+
+        configuration.captureTimeout = 120000;
+
+        configuration.customLaunchers = {
+            ChromeSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'OS X 10.11'
+            },
+            FirefoxSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'OS X 10.11'
+            }
+        };
+
+        configuration.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    } else {
+        configuration.browsers = [
             'ChromeCanary',
             'FirefoxDeveloper'
-        ],
+        ];
+    }
 
-        files: [
-            '../../src/module.js',
-            {
-                included: false,
-                pattern: '../../src/**/!(module).js',
-                served: false,
-                watched: true,
-            },
-            '../../test/integration/**/*.js',
-            '../../test/unit/**/*.js'
-        ],
-
-        frameworks: [
-            'browserify',
-            'mocha',
-            'sinon-chai' // implicitly uses chai too
-        ],
-
-        preprocessors: {
-            '../../src/module.js': 'browserify',
-            '../../test/integration/**/*.js': 'browserify',
-            '../../test/unit/**/*.js': 'browserify'
-        }
-
-    });
+    config.set(configuration);
 
 };

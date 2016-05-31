3ab2c4ce9031ae597e2c98b8da0f63f4ecd7b647
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-01 00:54:30 +0200

    namespace integration test

--- test/integration/module.js
+++ test/integration/module.js
@@ -2,22 +2,26 @@ import { connect } from '../../src/module';
 
 describe('module', () => {
 
-    it('should connect to a WebSocket and send and receive a messagge', function (done) {
-        connect('ws://echo.websocket.org')
-            .then((webSocketBroker) => webSocketBroker.register('a fake type'))
-            .then((webSocketSubject) => {
-                var message = { a: 'b', c: 'd' };
+    describe('connect()', () => {
 
-                webSocketSubject.subscribe({
-                    next (mssge) {
-                        expect(mssge).to.deep.equal(message);
+        it('should connect to a WebSocket and send and receive a messagge', function (done) {
+            connect('ws://echo.websocket.org')
+                .then((webSocketBroker) => webSocketBroker.register('a fake type'))
+                .then((webSocketSubject) => {
+                    var message = { a: 'b', c: 'd' };
 
-                        done();
-                    }
+                    webSocketSubject.subscribe({
+                        next (mssge) {
+                            expect(mssge).to.deep.equal(message);
+
+                            done();
+                        }
+                    });
+
+                    webSocketSubject.send(message);
                 });
+        });
 
-                webSocketSubject.send(message);
-            });
     });
 
 });

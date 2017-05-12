fb1f5fb68eb09f4c28eca3e0099ddcdbdac3ad21
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-12 17:49:08 +0200

    test empty messages

--- test/integration/module.js
+++ test/integration/module.js
@@ -82,91 +82,122 @@ describe('module', () => {
     describe('wrap()', () => {
 
         let dataChannelSubject;
-
-        let message;
-
         let remoteDataChannel;
 
         afterEach(() => dataChannelSubject.close());
 
-        beforeEach(() => {
-            message = { a: 'b', c: 'd' };
+        beforeEach(() => establishDataChannels()
+            .then(({ localDataChannel, remoteDataChannel: rmtDtChnnl }) => {
+                dataChannelSubject = wrap(localDataChannel);
+                remoteDataChannel = rmtDtChnnl;
+            }));
 
-            return establishDataChannels()
-                .then(({ localDataChannel, remoteDataChannel: rmtDtChnnl }) => {
-                    dataChannelSubject = wrap(localDataChannel);
-                    remoteDataChannel = rmtDtChnnl;
-                });
-        });
+        describe('with a message', () => {
 
-        it('should send and receive a messagge through an unmasked data channel', function (done) {
-            this.timeout(10000);
+            let message;
 
-            dataChannelSubject
-                .subscribe({
-                    next (mssge) {
-                        expect(mssge).to.deep.equal(message);
-
-                        done();
-                    }
-                });
-
-            remoteDataChannel.addEventListener('message', (event) => {
-                expect(event.data).to.equal('{"a":"b","c":"d"}');
-
-                remoteDataChannel.send(event.data);
+            beforeEach(() => {
+                message = { a: 'b', c: 'd' };
             });
 
-            dataChannelSubject.send(message);
-        });
+            it('should send and receive a messagge through an unmasked data channel', function (done) {
+                this.timeout(10000);
 
-        it('should send and receive a messagge through a masked data channel', function (done) {
-            this.timeout(10000);
+                dataChannelSubject
+                    .subscribe({
+                        next (mssge) {
+                            expect(mssge).to.deep.equal(message);
 
-            dataChannelSubject = dataChannelSubject
-                .mask({ a: 'fake mask' });
+                            done();
+                        }
+                    });
 
-            dataChannelSubject
-                .subscribe({
-                    next (mssge) {
-                        expect(mssge).to.deep.equal(message);
+                remoteDataChannel.addEventListener('message', (event) => {
+                    expect(event.data).to.equal('{"a":"b","c":"d"}');
 
-                        done();
-                    }
+                    remoteDataChannel.send(event.data);
                 });
 
-            remoteDataChannel.addEventListener('message', (event) => {
-                expect(event.data).to.equal('{"a":"fake mask","message":{"a":"b","c":"d"}}');
-
-                remoteDataChannel.send(event.data);
+                dataChannelSubject.send(message);
             });
 
-            dataChannelSubject.send(message);
-        });
+            it('should send and receive a messagge through a masked data channel', function (done) {
+                this.timeout(10000);
 
-        it('should send and receive a messagge through a deeply masked data channel', function (done) {
-            this.timeout(10000);
+                dataChannelSubject = dataChannelSubject
+                    .mask({ a: 'fake mask' });
 
-            dataChannelSubject = dataChannelSubject
-                .mask({ a: 'fake mask' })
-                .mask({ another: 'fake mask' });
+                dataChannelSubject
+                    .subscribe({
+                        next (mssge) {
+                            expect(mssge).to.deep.equal(message);
 
-            dataChannelSubject
-                .subscribe({
-                    next (mssge) {
-                        expect(mssge).to.deep.equal(message);
+                            done();
+                        }
+                    });
 
-                        done();
-                    }
+                remoteDataChannel.addEventListener('message', (event) => {
+                    expect(event.data).to.equal('{"a":"fake mask","message":{"a":"b","c":"d"}}');
+
+                    remoteDataChannel.send(event.data);
                 });
 
-            remoteDataChannel.addEventListener('message', (event) => {
-                expect(event.data).to.equal('{"a":"fake mask","message":{"another":"fake mask","message":{"a":"b","c":"d"}}}');
-
-                remoteDataChannel.send(event.data);
+                dataChannelSubject.send(message);
             });
 
-            dataChannelSubject.send(message);
+            it('should send and receive a messagge through a deeply masked data channel', function (done) {
+                this.timeout(10000);
+
+                dataChannelSubject = dataChannelSubject
+                    .mask({ a: 'fake mask' })
+                    .mask({ another: 'fake mask' });
+
+                dataChannelSubject
+                    .subscribe({
+                        next (mssge) {
+                            expect(mssge).to.deep.equal(message);
+
+                            done();
+                        }
+                    });
+
+                remoteDataChannel.addEventListener('message', (event) => {
+                    expect(event.data).to.equal('{"a":"fake mask","message":{"another":"fake mask","message":{"a":"b","c":"d"}}}');
+
+                    remoteDataChannel.send(event.data);
+                });
+
+                dataChannelSubject.send(message);
+            });
+
+        });
+
+        describe('without a message', () => {
+
+            it('should send and receive an empty messagge through a masked data channel', function (done) {
+                this.timeout(10000);
+
+                dataChannelSubject = dataChannelSubject
+                    .mask({ a: 'fake mask' });
+
+                dataChannelSubject
+                    .subscribe({
+                        next (message) {
+                            expect(message).to.be.undefined;
+
+                            done();
+                        }
+                    });
+
+                remoteDataChannel.addEventListener('message', (event) => {
+                    expect(event.data).to.equal('{"a":"fake mask"}');
+
+                    remoteDataChannel.send(event.data);
+                });
+
+                dataChannelSubject.send();
+            });
+
         });
 
     });

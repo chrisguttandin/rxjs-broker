a392c94706f4e60f23fc53777578f9c7f0f3b798
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-12-28 21:42:17 +0100

    fix: use only two ICE servers

--- test/helper/establish-data-channels.js
+++ test/helper/establish-data-channels.js
@@ -1,10 +1,7 @@
 const ICE_SERVERS = [ {
     urls: [
         'stun:stun.l.google.com:19302',
-        'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun3.l.google.com:19302',
-        'stun:stun4.l.google.com:19302'
+        'stun:global.stun.twilio.com:3478?transport=udp'
     ]
 } ];
 

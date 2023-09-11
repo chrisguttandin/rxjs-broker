2b1b045b77de7cd0115d37029520e2cd8c209b14
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-09-11 15:19:30 +0200

    test(config): update Karma config

--- config/karma/config-integration.js
+++ config/karma/config-integration.js
@@ -124,7 +124,7 @@ module.exports = (config) => {
                     browser: 'chrome',
                     captureTimeout: 300,
                     os: 'OS X',
-                    os_version: 'Big Sur' // eslint-disable-line camelcase
+                    os_version: 'Ventura' // eslint-disable-line camelcase
                 },
                 FirefoxBrowserStack: {
                     base: 'BrowserStack',
@@ -138,7 +138,7 @@ module.exports = (config) => {
                     browser: 'safari',
                     captureTimeout: 300,
                     os: 'OS X',
-                    os_version: 'Big Sur' // eslint-disable-line camelcase
+                    os_version: 'Ventura' // eslint-disable-line camelcase
                 }
             }
         });
